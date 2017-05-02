b4cdd049041a71db23fbf84e6438905e146e79fc
Bo Zhang <vampireb@gmail.com>
2017-05-02 07:28:16 -0400

    Step-3 init.

---
+++ app/AppBody.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AppBody extends Component {
+	render() {
+		return (
+			<div className="step">
+				Step 3
+			</div>
+		)
+	}
+}
+
---
+++ app/Step.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Step extends Component {
+	render() {
+		return (
+			<span className="step">
+				Step 3 completed!
+			</span>
+		)
+	}
+}
+
