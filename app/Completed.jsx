30ee5e218aaa889461896728bd1c9c3c776ce994
Bo Zhang <BZhang@globeandmail.com>
2017-05-05 02:20:29 -0400

    Fix indentation issues.

--- app/Completed.jsx
+++ app/Completed.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Completed extends Component {
-	render() {
-		return (
-			<span className="completed">
+  render() {
+    return (
+      <span className="completed">
         completed!
-			</span>
-		)
-	}
+      </span>
+    )
+  }
 }
 
